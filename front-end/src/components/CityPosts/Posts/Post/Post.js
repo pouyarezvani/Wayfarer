f15a7ec44a3f199aed4a7de06d49a4096cff5c45
pouyarezvani1 <pouyarezvani10@gmail.com>
2019-08-22 12:06:08 -0700

    Adds Posts & Post components

---
+++ front-end/src/components/CityPosts/Posts/Post/Post.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// Styles
+import '../../CityPosts.css'
+
+const Post = () => {
+    return (
+        <div className="post-box">
+            <div className="post-box-image">
+                <img src="#" alt="city"/>
+            </div>
+            <div className="post-box-content">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe, quiaudantium veritatis praesentium accusantium aliquam nulla ab est repudiandae! Minus ?</p>
+            </div>
+        </div>
+    );
+};
+
+export default Post;
---
+++ front-end/src/components/CityPosts/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// Internal Components
+import Post from './Post/Post';
+
+const Posts = () => {
+    
+    return (
+        <section className="posts-container">
+            <Post />
+        </section>
+    );
+};
+
+export default Posts;
