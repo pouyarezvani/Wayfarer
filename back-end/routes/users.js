7102de5b7119626879b95363ff14f2eed3e4e17c
meganecummings <megcummings@gmail.com>
2019-08-22 16:05:26 -0700

    UserController Complete

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -6,10 +6,10 @@ const authRequired = require('../middleware/authRequired');
 // Endpoint => 'api/v1/users'
 
 // USER controllers
-router.get('/:id', authRequired, ctrl.user.show),
+router.get('/:user_id', authRequired, ctrl.user.show),
 router.get('/', authRequired, ctrl.user.index);
-// router.get('/:slug/edit', authRequired, ctrl.user.edit);
-router.get('/:id/delete', authRequired, ctrl.user.delete);
+router.put('/:user_id', authRequired, ctrl.user.edit);
+router.delete('/:user_id', authRequired, ctrl.user.delete);
+router.post('/', authRequired, ctrl.user.create),
 
 module.exports = router;
-
