2d971901f7826044ef125110caca1c9d2ed94689
meganecummings <megcummings@gmail.com>
2019-08-22 11:51:05 -0700

    UserController CreateUser complete

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,20 +1,28 @@
 const db = require('../models');
 
-const show = (req, res) => {
+const showUser = (req, res) => {
     db.User.findById(req.params.id, { password: 0, _v: 0 }, (err, foundUser) => {
         if (err) return res.status(500).json({ status: 500, message: 'Something went wrong. Please try again.' });
         res.status(200).json({ status: 200, data: foundUser });
     });
 };
 
-const index = (req, res) => {
+const indexUsers = (req, res) => {
     db.User.find({}, { password: 0, _v: 0 }, (err, allUsers) => {
         if (err) return res.status(500).json({ status: 500, message: 'Something went wrong. Please try again.' });
         res.status(200).json({ status: 200, data: allUsers });
     });
 };
 
+const createUser = (req, res) => {
+    db.User.create(req.body, (err, newUser) => {
+        if (err) return res.sendStatus(400);
+        res.sendStatus(200);
+    });
+};
+
 module.exports = {
-    show, 
-    index
+    showUser, 
+    indexUsers,
+    createUser
 };
