27b8c9aef7f828e35e2aebc43cb2b9a79666f267
meganecummings <megcummings@gmail.com>
2019-08-22 10:35:27 -0700

    Post Model Complete

--- back-end/models/Post.js
+++ back-end/models/Post.js
@@ -1,6 +1,35 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const PostSchema = new mongoose.Schema({
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        requried: true,
+        maxlength: 200,
+        minlength: 1
+    },
+    user: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    content: {
+        type: String,
+        required: true, 
+        maxlength: 1000
+    },
+    city: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'City'
+        }
+    ],
+    date_posted: {
+        type: Date,
+        default: Date.now
+    }
+
 
 });
 
