531534759649b8315439894cefe6f1e5161fff8f
meganecummings <megcummings@gmail.com>
2019-08-22 11:32:08 -0700

    AuthController Verify Complete

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -83,10 +83,15 @@ const logout = (req, res) => {
     });
 };
 
-
+const verify = (req, res) => {
+    if (!req.session.currentUser) return res.status(401).json({ status: 401, message: 'Unauthorized. Please try again.' });
+    res.status(200).json({ status: 200, message: 'Current User Verified.' });
+}
 
 
 module.exports = {
     signup,
-    login
+    login,
+    logout,
+    verify
 };
