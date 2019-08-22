3561ebc73fe9fa5f739f79c0eebe5a28183e9216
meganecummings <megcummings@gmail.com>
2019-08-22 14:56:37 -0700

    Routes commented out until Controller completion

--- back-end/routes/cities.js
+++ back-end/routes/cities.js
@@ -6,9 +6,9 @@ const authRequired = require('../middleware/authRequired');
 // Endpoint : 'api/v1/cities'
 
 //User Profile
-router.get('./:slug', authRequired, ctrl.users.show);
-router.get('/', ctrl.users.index);
-router.get('./:slug/edit', authRequired, ctrl.users.edit);
-router.get('./:slug/delete', authRequired, ctrl.users.delete);
+// router.get('./:slug', authRequired, ctrl.users.show);
+// router.get('/', ctrl.users.index);
+// router.get('./:slug/edit', authRequired, ctrl.users.edit);
+// router.get('./:slug/delete', authRequired, ctrl.users.delete);
 
 module.exports = router;
--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -8,7 +8,7 @@ const authRequired = require('../middleware/authRequired');
 // USER controllers
 router.get('/:slug', authRequired, ctrl.users.show),
 router.get('/', authRequired, ctrl.users.index);
-router.get('/:slug/edit', authRequired, ctrl.user.edit);
+// router.get('/:slug/edit', authRequired, ctrl.user.edit);
 router.get('/:slug/delete', authRequired, ctrl.user.delete);
 
 module.exports = router;
