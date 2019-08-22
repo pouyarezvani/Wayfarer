1b2948ea3c8683e17b8cefe0930887ef7b5ced6b
meganecummings <megcummings@gmail.com>
2019-08-22 10:44:10 -0700

    City Model Complete

---
+++ back-end/models/City.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CitySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 30
+    },
+    image: {
+        type: String,
+        required: true
+    }, 
+    description: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+        required: true
+    }
+});
+
+const City = mongoose.model('City');
+
+module.exports = City;
