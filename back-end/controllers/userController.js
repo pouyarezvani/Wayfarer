75257129c90ad8c14f43d8eca3703dd3679a765b
meganecummings <megcummings@gmail.com>
2019-08-22 12:16:59 -0700

    UserController edit in process

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,7 +1,7 @@
 const db = require('../models');
 
 const showUser = (req, res) => {
-    db.User.findById(req.params.id, { password: 0, _v: 0 }, (err, foundUser) => {
+    db.User.findOne(req.params.username, { password: 0, _v: 0 }, (err, foundUser) => {
         if (err) return res.status(500).json({ status: 500, message: 'Something went wrong. Please try again.' });
         res.status(200).json({ status: 200, data: foundUser });
     });
@@ -15,11 +15,13 @@ const indexUsers = (req, res) => {
 };
 
 const createUser = (req, res) => {
-    db.User.create(req.body, (err, newUser) => {
+    db.User.create(req.body, (err, createdUser) => {
         if (err) return res.sendStatus(400);
-        res.sendStatus(200);
+        res.sendResponse(res, createdUser);
     });
 };
+
+
 
 module.exports = {
     showUser, 
