264c3aee2b5f5beeedf998ab46c753f6ce1a8684
edfranco <edfranco415@gmail.com>
2019-08-22 13:54:59 -0700

    created more examples

--- front-end/src/components/Aside/Aside.js
+++ front-end/src/components/Aside/Aside.js
@@ -5,20 +5,14 @@ import City from '../City/City';
 // styles
 import './Aside.css';
 
-const Aside = () => {
-    const cities = [
-        <Link to="cities/sanfrancisco">
-            <City cityName="San Francisco" imageUrl="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/San_Francisco_from_the_Marin_Headlands_in_March_2019.jpg/250px-San_Francisco_from_the_Marin_Headlands_in_March_2019.jpg" />
-        </Link >,
-        <Link to="cities/austin">
-            <City cityName="Austin" imageUrl="https://static.parade.com/wp-content/uploads/2018/11/austin-texas-skyline-state-flag-ftr.jpg" />
-        </Link>,
-        <Link to="cities/guadalajara">
-            <City cityName="Guadalajara" imageUrl="https://dynaimage.cdn.cnn.com/cnn/q_auto,w_634,c_fill,g_auto,h_357,ar_16:9/http%3A%2F%2Fcdn.cnn.com%2Fcnnnext%2Fdam%2Fassets%2F180514093837-04-guadalajara-city-guide.jpg" />
-        </Link>,
-
-
-    ]
+const Aside = ({ cities }) => {
+    const citiesArr = cities.map(city => {
+        return (
+            <Link key={city.id} to={city.name} >
+                <City key={city.id} name={city.name} image={city.imageUrl} />
+            </Link>
+        )
+    })
 
     return (
         <div className="aside">
@@ -26,7 +20,7 @@ const Aside = () => {
                 <h1>Cities</h1>
             </div>
             <div className="asides-cities">
-                {cities}
+                {citiesArr}
             </div>
 
         </div>
