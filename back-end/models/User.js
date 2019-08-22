3b017d1dad00b4b960fbe734001b6d1b9178686f
meganecummings <megcummings@gmail.com>
2019-08-22 10:27:26 -0700

    User Model Complete

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -4,12 +4,45 @@ const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         unique: true,
-        required: true
+        required: true, 
+        maxlength: 30,
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        maxlength: 30,
+        minlength: 3
+    }, 
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }, 
+    date_joined: {
+        type: Date,
+        default: Date.now
+    },
+    password: {
+        type: String,
+        required: true,
+        maxlength: 30,
+        minlength: 3
+    }, 
+    admin: {
+        type: Boolean
+    }, 
+    image_url: {
+        type: String,
+        default: "https://thesocietypages.org/socimages/files/2009/05/vimeo.jpg"
+    }, 
+    slug: {
+        type: String
+    }, 
+    current_city: {
+        type: String
     }
 });
 
-module.exports = mongoose.model('User', UserSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
