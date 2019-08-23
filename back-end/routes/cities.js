bd7ab3d7a274fc54ee8a86f52f4ff94aed1f65be
edfranco <edfranco415@gmail.com>
2019-08-23 08:33:50 -0700

    debugged the server

--- back-end/routes/cities.js
+++ back-end/routes/cities.js
@@ -8,8 +8,8 @@ const authRequired = require('../middleware/authRequired');
 //City Routes
 router.get('./:city_id', ctrl.city.show);
 router.get('/', ctrl.city.index);
-router.put('./:city_id', authRequired, ctrl.city.edit);
-router.delete('./:city_id', authRequired, ctrl.city.delete);
+router.put('./:city_id', authRequired, ctrl.city.update);
+router.delete('./:city_id', authRequired, ctrl.city.remove);
 router.post('/', authRequired, ctrl.city.create),
 
-module.exports = router;
+    module.exports = router;
