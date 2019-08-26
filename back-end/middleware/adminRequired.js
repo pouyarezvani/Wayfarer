de92c78af49ad5bba6de0403ac90f403e01eb188
meganecummings <megcummings@gmail.com>
2019-08-25 18:02:54 -0700

    Updated Middleware

--- back-end/middleware/adminRequired.js
+++ back-end/middleware/adminRequired.js
@@ -1,7 +1,7 @@
 // checks for sessions and returns 401 otherwise 
 
 module.exports = (req, res, next) => {
-    if (req.session.currentUser.admin) {
+    if (!req.body.admin) {
       return res.status(401).json({ status: 401, message: 'Unauthorized. Please login and try again' });
     }
     next();
