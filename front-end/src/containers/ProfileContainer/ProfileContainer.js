b8b9bc413fa8c3bd8b0b59c396f777b15833a280
meganecummings <megcummings@gmail.com>
2019-08-27 08:34:13 -0700

    Reverted to Post View w/out filtered

--- front-end/src/containers/ProfileContainer/ProfileContainer.js
+++ front-end/src/containers/ProfileContainer/ProfileContainer.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import { API_URL } from '../../constants';
 import Profile from '../../components/Profile/Profile';
+import CityPosts from '../../components/CityPosts/CityPosts';
 import './ProfileContainer.css'
-import PostView from './PostView';
 
 class ProfileContainer extends Component {
     state = {
@@ -26,25 +26,18 @@ class ProfileContainer extends Component {
         this.deletePost();
     }
 
-    submitPost = event => {
-        event.preventDefault();
-        axios.post(`${API_URL}/posts/`, {
-            username: this.state.user.username,
-            // city_slug: this.props.slug,
-            title: this.state.title,
-            content: this.state.content
-        }, { withCredentials: true })
-            .then(res => {
-                console.log(res);
-            })
-            .catch(error => {
-                console.log(error.response);
-            });
-    }
+    // deletePost = (event) => {
+    //     event.preventDefault();
+    //     console.log(this);
+    //     axios.delete(`${API_URL}/posts/${this.props.deletePost}`)
+    //         .then(response => console.log(response))
+    //         .catch(error => console.log(error.response));
+    // }
 
     componentDidMount() {
         this.getUserInfo();
         this.getPosts();
+
     };
 
     // Axios call to get posts
@@ -57,7 +50,7 @@ class ProfileContainer extends Component {
             // console.log(this.state.posts)
             this.state.posts.data.map((post) => {
                 if (post.username === this.state.profile.data.username) {
-                    return this.state.postsFiltered.push(post)
+                    this.state.postsFiltered.push(post)
                 }
                 })
                 console.log(this.state.postsFiltered)
@@ -68,9 +61,6 @@ class ProfileContainer extends Component {
     
 
     render() {
-        console.log(this.state.postsFiltered)
-        console.log(this.state.postsFiltered)
-
         return (
             <div className="post-container">
                 {this.state.errors && this.state.errors.map((error, index) => (
@@ -80,16 +70,13 @@ class ProfileContainer extends Component {
                 ))}
                 {this.state.profile && <Profile user={this.state.profile.data}/>}
                 <div className="post-container">
-                    {this.state.postsFiltered && this.state.postsFiltered.map((foundPost) => {
-                        return (
-                        <div className="post-container">
-                        <h3>Title: {foundPost.title}</h3>
-                        <p>Author: {foundPost.username}</p>
-                        <p>Content: {foundPost.content}</p>
-                        <p>Date Posted: {foundPost.date_posted}</p>
-                        </div>
-                        )
-                    })}
+                    <h2>Your Posts</h2>
+                    {this.state.posts.data && this.state.posts.data.map(foundPost => (
+                    <div className="post" key={foundPost}><h4>{foundPost.title} </h4>
+                    <p>{foundPost.content}</p>
+                    <p> Author: {foundPost.username}</p>
+                    </div>
+                    ))}
                 </div>
             </div>
         );
